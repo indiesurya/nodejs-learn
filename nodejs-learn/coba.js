09b7404549988b0bfd085ad35980d7550b3e2977
indiesurya <putuindie11@gmail.com>
2022-10-08 08:57:21 +0800

    export module

---
+++ nodejs-learn/coba.js
@@ -0,0 +1,7 @@
+// console.log('Hello World!');
+
+const sayHello = (nama) =>  {
+    console.log(`Nama saya ${nama}`);
+}
+
+module.exports = sayHello; // export to another file to use this function
---
+++ nodejs-learn/index.js
@@ -0,0 +1,13 @@
+// console.log('Hello Good Morning Indie!');
+// const sayHello = (name) => {
+//     return `Hello my name is ${name}`;
+// }
+
+// console.log(sayHello('Indie'));
+
+const sayHello = require('./coba') // call module from coba.js
+
+sayHello('Indie'); // call function sayHello from coba.js
+
+
+

0ce9384270dacfe72465ad7f699058840c505290
indiesurya <putuindie11@gmail.com>
2022-10-08 08:30:37 +0800

    execute from command line or terminal

---
+++ coba.js
@@ -0,0 +1,5 @@
+const nama = 'Indie';
+const umur = 22;
+const sayHi = (nama, umur) => {
+	console.log(`NAMA SAYA ${nama},UMUR SAYA ${umur} TAHUN`)
+}
---
+++ coba2.js
@@ -0,0 +1,7 @@
+const nama = 'Indie'
+nama
+const umur = 22
+umur
+const sayHi = (nama, umur) => {
+console.log(`nama saya ${nama}, umur saya ${umur} tahun`)
+}
